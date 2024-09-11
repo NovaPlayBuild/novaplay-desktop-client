7c36052c333c237289c626beb1935ef4c74d10fa
NovaPlay <broke333boi@proton.me>
2024-09-11 11:21:19 +0300

    Replace HyperPlay with NovaPlay in src/frontend/screens/Login/components/LoginWarning/index.tsx

--- src/frontend/screens/Login/components/LoginWarning/index.tsx
+++ src/frontend/screens/Login/components/LoginWarning/index.tsx
@@ -8,7 +8,7 @@ import {
 import { useTranslation } from 'react-i18next'
 import { epicLoginPath, gogLoginPath } from '../..'
 import { NavLink } from 'react-router-dom'
-import { Button } from '@hyperplay/ui'
+import { Button } from '@novaplay/ui'
 
 interface LoginWarningProps {
   warnLoginForStore: null | 'epic' | 'gog'
@@ -30,13 +30,13 @@ const LoginWarning = function ({
   if (warnLoginForStore === 'epic') {
     textContent = t(
       'not_logged_in.epic',
-      "You are not logged in with an Epic account in HyperPlay. Don't use the store page to login, click the following button instead:"
+      "You are not logged in with an Epic account in NovaPlay. Don't use the store page to login, click the following button instead:"
     )
     loginPath = epicLoginPath
   } else if (warnLoginForStore === 'gog') {
     textContent = t(
       'not_logged_in.gog',
-      "You are not logged in with a GOG account in HyperPlay. Don't use the store page to login, click the following button instead:"
+      "You are not logged in with a GOG account in NovaPlay. Don't use the store page to login, click the following button instead:"
     )
     loginPath = gogLoginPath
   }
