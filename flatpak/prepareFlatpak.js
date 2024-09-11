8dd7eacb1b34cb74641416ea92a59b235c3e4df6
NovaPlay <broke333boi@proton.me>
2024-09-11 10:40:27 +0300

    Replace HyperPlay with NovaPlay in flatpak/prepareFlatpak.js

--- flatpak/prepareFlatpak.js
+++ flatpak/prepareFlatpak.js
@@ -13,9 +13,9 @@ async function main() {
     let placeholder = ""
     let releaseTime = ""
     if (process.argv[2] === "release") {
-        const { data } = await axios.get("https://api.github.com/repos/HyperPlay-Gaming/hyperplay-desktop-client/releases/latest")
+        const { data } = await axios.get("https://api.github.com/repos/NovaPlay-Gaming/novaplay-desktop-client/releases/latest")
         const tarxz = data.assets.find((asset) => asset.browser_download_url.includes("tar.xz"))
-        const outputFile = `${os.tmpdir()}/hyperplay.tar.xz`
+        const outputFile = `${os.tmpdir()}/novaplay.tar.xz`
         child_process.spawnSync("curl", ["-L", tarxz.browser_download_url, "-o", outputFile, "--create-dirs"])
         const outputContent = fs.readFileSync(outputFile)
         const hashSum = crypto.createHash('sha512');
@@ -32,7 +32,7 @@ async function main() {
     } else {
         placeholder = [
             "type: file",
-            `path: "../dist/hyperplay-${package.version}.tar.xz"`
+            `path: "../dist/novaplay-${package.version}.tar.xz"`
         ].join("\n        ")
         releaseTime = new Date().toISOString().split('T')[0]
     }
@@ -43,18 +43,18 @@ async function main() {
     }
 
     // generate manifest
-    let templateManifest = fs.readFileSync(`./flatpak/templates/xyz.hyperplay.HyperPlay.yml.template`, { encoding: 'utf-8' })
-    templateManifest = templateManifest.replace("${hyperplay-tarxz}", placeholder)
-    fs.writeFileSync("./flatpak-build/xyz.hyperplay.HyperPlay.yml", templateManifest)
+    let templateManifest = fs.readFileSync(`./flatpak/templates/xyz.novaplay.NovaPlay.yml.template`, { encoding: 'utf-8' })
+    templateManifest = templateManifest.replace("${novaplay-tarxz}", placeholder)
+    fs.writeFileSync("./flatpak-build/xyz.novaplay.NovaPlay.yml", templateManifest)
 
     // generate metainfo
-    let templateMetaInfo = fs.readFileSync(`./flatpak/templates/xyz.hyperplay.HyperPlay.metainfo.xml.template`, { encoding: 'utf-8' })
-    templateMetaInfo = templateMetaInfo.replace("${hyperplay-version}", `v${package.version}`).replace("${hyperplay-release-date}", releaseTime)
-    fs.writeFileSync("./flatpak-build/xyz.hyperplay.HyperPlay.metainfo.xml", templateMetaInfo)
+    let templateMetaInfo = fs.readFileSync(`./flatpak/templates/xyz.novaplay.NovaPlay.metainfo.xml.template`, { encoding: 'utf-8' })
+    templateMetaInfo = templateMetaInfo.replace("${novaplay-version}", `v${package.version}`).replace("${novaplay-release-date}", releaseTime)
+    fs.writeFileSync("./flatpak-build/xyz.novaplay.NovaPlay.metainfo.xml", templateMetaInfo)
 
     // copy extra files
-    fs.copyFileSync("./flatpak/xyz.hyperplay.HyperPlay.desktop", "./flatpak-build/xyz.hyperplay.HyperPlay.desktop")
-    fs.copyFileSync("./flatpak/xyz.hyperplay.HyperPlay.png", "./flatpak-build/xyz.hyperplay.HyperPlay.png")
+    fs.copyFileSync("./flatpak/xyz.novaplay.NovaPlay.desktop", "./flatpak-build/xyz.novaplay.NovaPlay.desktop")
+    fs.copyFileSync("./flatpak/xyz.novaplay.NovaPlay.png", "./flatpak-build/xyz.novaplay.NovaPlay.png")
     fs.copyFileSync("./flatpak/flathub.json", "./flatpak-build/flathub.json")
     fs.cpSync("./flatpak/patches", "./flatpak-build/patches", { recursive: true })
 }
