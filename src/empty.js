f480fb161f8ca8aae4831f140e4e5997a8c0920b
NovaPlay <broke333boi@proton.me>
2024-09-11 14:06:49 +0300

    Update empty.js

--- src/empty.js
+++ src/empty.js
@@ -1,2 +1,2 @@
-// this file is imported in place of missing optional dependencies to fix vite build errors
+//  file is imported in place of missing optional dependencies to fix vite build errors
 module.exports = {}
