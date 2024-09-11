4a1d71e975f6ce4e381f33bf916d37ae516c1f31
NovaPlay <broke333boi@proton.me>
2024-09-11 10:48:50 +0300

    Replace HyperPlay with NovaPlay in src/backend/overlay/index.ts

--- src/backend/overlay/index.ts
+++ src/backend/overlay/index.ts
@@ -1,24 +1,24 @@
 import { LogPrefix, logError } from 'backend/logger/logger'
-import { HyperPlayAPI } from '@hyperplay/utils'
+import { NovaPlayAPI } from '@novaplay/utils'
 
 export async function getHpOverlay() {
   try {
-    const hpOverlay = await import('@hyperplay/overlay')
+    const hpOverlay = await import('@novaplay/overlay')
     if (!Object.hasOwn(hpOverlay, 'toggleOverlay')) {
       return undefined
     }
     return hpOverlay
   } catch (err) {
-    logError(`Error getting overlay ${err}`, LogPrefix.HyperPlay)
+    logError(`Error getting overlay ${err}`, LogPrefix.NovaPlay)
   }
   return undefined
 }
 
-export const initOverlay = async function (api: HyperPlayAPI) {
+export const initOverlay = async function (api: NovaPlayAPI) {
   try {
     const hpOverlay = await getHpOverlay()
     hpOverlay?.initOverlay(api)
   } catch (err) {
-    logError(`Error initializing overlay ${err}`, LogPrefix.HyperPlay)
+    logError(`Error initializing overlay ${err}`, LogPrefix.NovaPlay)
   }
 }
